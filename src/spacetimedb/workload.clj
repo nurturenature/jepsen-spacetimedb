efa6499a7954fa3e35fa13fcdf6026636d5acd1a
Jeff Suttor <jeff.suttor@earthlink.net>
2026-03-24 19:54:15 -0700

    wrap our client in a Jepsen client/timeout, stronger guarantee to timeout vs the http client

--- src/spacetimedb/workload.clj
+++ src/spacetimedb/workload.clj
@@ -1,17 +1,18 @@
 (ns spacetimedb.workload
-  (:require [jepsen.tests.cycle.append :as list-append]
+  (:require [jepsen.client :refer [timeout]]
+            [jepsen.tests.cycle.append :as list-append]
             [spacetimedb.role :as role]))
 
 (defn list-append
   "A SpacetimeDB workload where all reads/writes to a keyed append only list
    happen in a transaction in a Procedure."
-  [{:keys [key-count min-txn-length max-txn-length max-writes-per-key] :as opts}]
+  [{:keys [client-timeout key-count min-txn-length max-txn-length max-writes-per-key] :as opts}]
   (assert (and key-count min-txn-length max-txn-length max-writes-per-key)
           (str "opts must specify {key-count min-txn-length max-txn-length max-writes-per-key}: " opts))
   (merge
    (list-append/test opts)
    {:db     (role/roles-based-db opts)
-    :client (role/restricted-client)
+    :client (timeout (* 1000 client-timeout) (role/restricted-client))
     :roles  (role/roles-map opts)
     :spacetimedb {:table "lists" :dispatch-by-f {:txn ["procedure"]}}}))
 
